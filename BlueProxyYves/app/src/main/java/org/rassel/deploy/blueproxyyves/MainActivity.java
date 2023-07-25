f433b0e889fc4eccb611cc61ffa5873a871ebc09
Dominic Marcinkowski <d.marcinkowski@albellus.com>
2023-07-25 16:25:33 +0200

    quick and dirty GPS + file write

--- BlueProxyYves/app/src/main/java/org/rassel/deploy/blueproxyyves/MainActivity.java
+++ BlueProxyYves/app/src/main/java/org/rassel/deploy/blueproxyyves/MainActivity.java
@@ -1,29 +1,70 @@
 package org.rassel.deploy.blueproxyyves;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
-import android.util.Log;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
-public class MainActivity extends Activity {
+
+import android.location.Location;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends Activity implements LocationListener {
     private static final int UDP_PORT = 61283;
     private TextView dataTextView;
     private Handler handler = new Handler(Looper.getMainLooper());
+    private LocationManager locationManager;
+
+
+    private Location location = null;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        context = getApplicationContext();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        this.location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
         setContentView(R.layout.activity_main);
 
         dataTextView = findViewById(R.id.dataTextView);
 
         // Starten Sie den UDP-Listener in einem separaten Thread
         new Thread(new UdpListenerThread()).start();
+
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        this.location = location;
     }
 
     private class UdpListenerThread implements Runnable {
@@ -35,6 +76,9 @@ public class MainActivity extends Activity {
                 byte[] buffer = new byte[1024];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
+
+                dataTextView.setText("Warte auf Nachricht");
+
                 while (true) {
                     socket.receive(packet);
                     final String receivedMessage = new String(buffer, 0, packet.getLength());
@@ -42,15 +86,41 @@ public class MainActivity extends Activity {
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
+
+                            dataTextView.setText("Nachricht empfangen.");
                             // Zeigen Sie die empfangene Nachricht in der TextView an
-                            Log.d("UDP_DEBUG", "ja");
-                            dataTextView.setText("Empfangene Nachricht" + receivedMessage);
+                            dataTextView.append("\n" + receivedMessage);
+                            if (location != null) {
+                                dataTextView.append("\n" + location.getLatitude() + "\n" + location.getLongitude());
+                                writeToFile(System.currentTimeMillis() + "\t" + receivedMessage.substring(7) + "\t" + location.getLatitude() + "\t" + location.getLongitude() + "\n");
+                            } else {
+                                dataTextView.append("\nNo location.");
+                            }
                         }
                     });
                 }
             } catch (Exception e) {
                 e.printStackTrace();
+
+                dataTextView.setText("Error: " + e.getMessage());
+            }
+        }
+
+        private void writeToFile(String data) {
+            try {
+                File p = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                dataTextView.append("\nTry writing file");
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "loraLog.csv"), true));
+                outputStreamWriter.write(data);
+                outputStreamWriter.close();
+                dataTextView.append("\nFile written.");
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                dataTextView.append("\nError writing file: " + e.getMessage());
             }
         }
     }
+
+
 }
